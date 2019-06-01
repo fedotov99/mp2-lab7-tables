47f471d0648963f25812fee8b0b39a21c4d50e76
Vlad Fedotov <fedotovvlad@list.ru>
2019-06-01 16:40:36 +0300

    Datvalue.h added

---
+++ include/DatValue.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class TDatValue;
+typedef TDatValue * PTDatValue;
+
+class TDatValue {  //модуль, объ€вл€ющий абстрактный класс объектов-значений списка
+  public:
+    virtual TDatValue * GetCopy() =0; // создание копии
+   ~TDatValue() {}
+};
