0e702996f5fe3762a5e253f40f0b40895b271c82
Vlad Fedotov <fedotovvlad@list.ru>
2019-06-02 20:05:04 +0300

    THashTable added

--- include/THashTable.h
+++ include/THashTable.h
@@ -1,6 +1,11 @@
+#pragma once
+#include "TTable.h"
+
+class TTabRecord;
+
 class  THashTable : public TTable {
   protected:
-    virtual unsigned long HashFunc(const Tkey key); // hash-функция
+    virtual unsigned long HashFunc(const TKey key); // hash-функция
   public:
-    THashTable() : TTable() {} //конструктор
+    THashTable() : TTable() {}
 };	
